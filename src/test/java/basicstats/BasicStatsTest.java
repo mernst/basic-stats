105b8c76808d1d28967422ff86040893b5c30193
Michael Ernst <mernst@cs.washington.edu>
2024-01-17 16:54:05 -0800

    Added a first test case for central tendency measures.

---
+++ src/test/java/basicstats/BasicStatsTest.java
@@ -0,0 +1,22 @@
+package basicstats;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class BasicStatsTest {
+  private static double EPS = 1e-9;
+
+  @Test
+  public void testCentralTendency() {
+    double[] numbers = {2, 2, 3, 3, 3, 4, 4};
+    double mean = BasicStats.mean(numbers);
+    assertEquals(3, mean, EPS);
+    /* TODO: Not yet implemented!
+            double median = BasicStats.median(numbers);
+            assertEquals (3, median, EPS);
+            double mode   = BasicStats.mode(numbers);
+            assertEquals (3, mode, EPS);
+    */
+  }
+}
